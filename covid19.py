ee895a6d782392cb3b98d34c7cd898604c2bfb94
Turdibek <turdibekdev05@gmail.com>
2022-03-25 06:43:21 +0500

    COVID-19

--- covid19.py
+++ covid19.py
@@ -51,16 +51,47 @@ class COVID19:
         self.recovery_rate = self.recovered.split(' ')[0]
         self.active_rate = self.active.split(' ')[0]
         return f'Confirmed: {self.confirmed}\nDeaths: {self.deaths}\nRecovered: {self.recovered}\nActive: {self.active}\nDeath Rate: {self.death_rate}\nRecovery Rate: {self.recovery_rate}\nActive Rate: {self.active_rate}'
-        self.country_url = f'https://www.worldometers.info/coronavirus/country/{country}/'
-        self.country_response = requests.get(self.country_url)
-        self.country_soup = BS(self.country_response.text, 'html.parser')
-        self.country_data = self.country_soup.find_all(
-            'div', class_='maincounter-number')
-        self.country_confirmed = self.country_data[0].text.strip()
-        self.country_deaths = self.country_data[1].text.strip()
-        self.country_recovered = self.country_data[2].text.strip()
-        self.country_active = self.country_confirmed.split(' ')[0]
-        self.country_death_rate = self.country_deaths.split(' ')[0]
-        self.country_recovery_rate = self.country_recovered.split(' ')[0]
-        self.country_active_rate = self.country_active.split(' ')[0]
-        return f'Confirmed: {self.country_confirmed}\nDeaths: {self.country_deaths}\nRecovered: {self.country_recovered}\nActive: {self.country_active}\nDeath Rate: {self.country_death_rate}\nRecovery Rate: {self.country_recovery_rate}\nActive Rate: {self.country_active_rate}'
+
+    def get_total_deaths(self):
+        """
+        Butun dunyoda o'limlar
+
+        ```python
+        covid19 = COVID19()
+        print(covid19.get_total_deaths())
+        ```
+        """
+        return self.deaths
+
+    def get_total_recovered(self):
+        """
+        Butun dunyoda tuzalganlar
+
+        ```python
+        covid19 = COVID19()
+        print(covid19.get_total_recovered())
+        ```
+        """
+        return self.recovered
+
+    def get_total_confirmed(self):
+        """
+        Butun dunyoda kasallanganlar
+
+        ```python
+        covid19 = COVID19()
+        print(covid19.get_total_confirmed())
+        ```
+        """
+        return self.confirmed
+
+    def get_total_active(self):
+        """
+        Butun dunyoda faoliyatlar
+
+        ```python
+        covid19 = COVID19()
+        print(covid19.get_total_active())
+        ```
+        """
+        return self.active
